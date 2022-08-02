d740548d3d90e452a27157052a7a1203e7c55326
chandrashekaranaik122 <110366329+chandrashekaranaik122@users.noreply.github.com>
2022-08-02 08:15:14 +0530

    Add files via upload

---
+++ president_height.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+
+
+# In[3]:
+
+
+pd.read_csv(r'C:\Users\chand\OneDrive\Desktop\president_heights.csv')
+
+
+# In[ ]:
+
+
+
+
